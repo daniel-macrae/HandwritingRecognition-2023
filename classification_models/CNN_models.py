0ba6e33bf568e7137d08387642190de3217ca779
SaraIglesiasSN <sara.iglesias4@gmail.com>
2023-06-06 21:17:58 +0200

    lastclass

--- classification_models/CNN_models.py
+++ classification_models/CNN_models.py
@@ -5,18 +5,8 @@ class CharacterCNN(nn.Module):
     def __init__(self, numChannels = 1, classes = 27, batch_size = 16, dropout_rate = 0.5):
         super(CharacterCNN, self).__init__()
 
-        """
-        self.conv1 = nn.Conv2d(numChannels, 32, kernel_size=5, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(32, BATCH_SIZE, kernel_size=5, stride=1, padding=1)
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.fc1 = nn.Linear( BATCH_SIZE *12* 12, 128) #this shoulf be BATCH_SIZE *12* 12 but that doesnt work
-        self.fc2 = nn.Linear(128, classes)
-        self.relu = nn.ReLU() # could also just use nn.functional.relu
-        #self.logSoftmax = nn.LogSoftmax(dim=1)
-        """    
-
-        self.conv1 = nn.Conv2d(numChannels, 32, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(32, batch_size, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(numChannels, 32, kernel_size=7, stride=1, padding=2)
+        self.conv2 = nn.Conv2d(32, batch_size, kernel_size=7, stride=1, padding=2)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
         #self.fc1 = nn.Linear( batch_size *12* 12, 64)  # 50x 50 input
         self.fc1 = nn.Linear( batch_size *8* 8, 64)  # 32x 32 input
@@ -34,7 +24,7 @@ class CharacterCNN(nn.Module):
         x = self.pool(self.relu(self.conv2(x)))
         x = self.dropout(x)
         #x = torch.flatten(x, 1)
-        #print(x.shape)
+        print(x.shape)
         x = x.view(x.size(0), -1) #same as flatten
         x = self.relu(self.fc1(x))
         x = self.dropout(x)
@@ -42,28 +32,6 @@ class CharacterCNN(nn.Module):
         #output = self.logSoftmax(output)
         return output
 
-
-class DanNet1(nn.Module):
-    def __init__(self, num_classes=27, batch_size = 16, dropout_rate = 0.5):
-        super(DanNet1, self).__init__() 
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(1, batch_size, kernel_size=3, stride=1, padding=0),
-            nn.BatchNorm2d(batch_size),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size = 2, stride = 2))
-        #self.fc = nn.Linear(batch_size *24 *24, 128) # 50x50 input
-        self.fc = nn.Linear(batch_size *15 *15, 128) # 32x32 input
-        self.relu = nn.ReLU()
-        self.fc1 = nn.Linear(128, num_classes)
-        
-    def forward(self, x):
-        out = self.layer1(x)
-        #print(out.shape)
-        out = out.reshape(out.size(0), -1)
-        out = self.fc(out)
-        out = self.relu(out)
-        out = self.fc1(out)
-        return out
 
 
 class LeNet5(nn.Module):
@@ -87,11 +55,40 @@ class LeNet5(nn.Module):
         
     def forward(self, x):
         out = self.layer1(x)
+        out = self.dropout(x)
         out = self.layer2(out)
+        out = self.dropout(out)
         out = out.reshape(out.size(0), -1)
         out = self.fc(out)
         out = self.relu(out)
         out = self.fc1(out)
         out = self.relu1(out)
+        out = self.dropout(out)
         out = self.fc2(out)
+        return out
+    
+
+
+class DanNet1(nn.Module):
+    def __init__(self, num_classes=27, batch_size = 16, dropout_rate = 0.5):
+        super(DanNet1, self).__init__() 
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, batch_size, kernel_size=3, stride=1, padding=0),
+            nn.BatchNorm2d(batch_size),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        #self.fc = nn.Linear(batch_size *24 *24, 128) # 50x50 input
+        self.fc = nn.Linear(batch_size *15 *15, 128) # 32x32 input
+        self.relu = nn.ReLU()
+        self.fc1 = nn.Linear(128, num_classes)
+        
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.dropout(x)
+        #print(out.shape)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+        out = self.relu(out)
+        out = self.dropout(out)
+        out = self.fc1(out)
         return out
--- dssRecognition.py
+++ dssRecognition.py
@@ -197,7 +197,7 @@ def get_args_parser(add_help=True):
     parser.add_argument("--input", default="Data/image-data", type=str, help="path to input image or folder of images")
     parser.add_argument("--output_folder", default="Results", type=str, help="folder to save the results in")
     
-    parser.add_argument("--debugging", default=False, type=bool, help="whether to save images of the intermediate steps")
+    parser.add_argument("--debugging", default=True, type=bool, help="whether to save images of the intermediate steps")
     parser.add_argument("--debugging_folder", default="debug", type=str, help="folder to save the debugging images in")
 
     return parser
